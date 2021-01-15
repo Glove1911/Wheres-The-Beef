3e836ceddba272be840654b71cf5d44877367da9
Harrison Glover <harrison_glover@yahoo.com>
2021-01-15 00:35:08 -0600

    updated default values for burgers

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(255),
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN 
     PRIMARY KEY (id)
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-    INSERT INTO burgers (burger_name,devoured) VALUES ("Bacon Cheesburger");
-    INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
-    INSERT INTO burgers(burger_name, devoured) VALUES ("Double Cheesburger");
+    INSERT INTO burgers (burger_name,devoured) VALUES ("Bacon Cheesburger",false);
+    INSERT INTO burgers (burger_name,devoured) VALUES ("Veggie Burger",false);
+    INSERT INTO burgers(burger_name, devoured) VALUES ("Double Cheesburger",false);
