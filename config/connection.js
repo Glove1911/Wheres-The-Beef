54f4caa97720042dcf9d0c618702270b6aab9a9f
Harrison Glover <harrison_glover@yahoo.com>
2021-01-16 09:55:27 -0600

    corrected styling for button

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
 
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: 3306,
   user: process.env.DB_USER,
